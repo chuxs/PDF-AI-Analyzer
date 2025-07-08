1ada74373198ff0e71652e35d32d6e4911dc3e94
chuxs <chuksanyaanya@gmail.com>
2025-07-08 10:49:56 +0100

    working on pdf display

--- Public/js/script.js
+++ Public/js/script.js
@@ -5,11 +5,11 @@ function handleFiles() {
     const fileList = this.files[0]; /* now you can work with the file list */
     console.log(fileList.name);
 
-    pdfName = document.getElementById("pdf-name");
+    const pdfName = document.getElementById("pdf-name");
     pdfName.innerText = fileList.name;
   
     const actualPdfFIle = URL.createObjectURL(fileList);
-    pdfDisplay = document.getElementById("image-content-id");
+    const pdfDisplay = document.getElementById("pdf-content-id");
     pdfDisplay.src = actualPdfFIle;
 
 }
