00b0e9903143aecbc8c54d86058ba20b3812d121
chuxs <chuksanyaanya@gmail.com>
2025-07-11 16:21:23 +0100

    imlpmenting post to firebase

--- index.js
+++ index.js
@@ -12,11 +12,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// var imageServer = "";
 const API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
 const API_KEY = "AIzaSyAKOhMfY55r1UpBEGIQ7a5cazUDJTP3RVg"
-// const API_KEY = process.env.API_KEY;
-const ai = new GoogleGenAI({ apiKey: API_KEY });
 
 const upload = multer({ storage: multer.memoryStorage() });
 
@@ -27,7 +24,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "Public")));
 
 const firebaseConfig = {
-  // ...
+  apiKey: "AIzaSyBLVVVoUjTV8vXJ2nrOSUbiDz1cLLrGlA0",
+  authDomain: "temporary-pdf-store.firebaseapp.com",
+  projectId: "temporary-pdf-store",
+  storageBucket: "temporary-pdf-store.firebasestorage.app",
+  messagingSenderId: "626204259528",
+  appId: "1:626204259528:web:f03ab3818262122e5ee1fd",
+  measurementId: "G-EYG4BR43TD",
   storageBucket: 'temporary-pdf-store.firebasestorage.app'
 };
 
@@ -39,6 +42,10 @@ const storage = getStorage(storeBucket);
 
 app.get("/", (req, res) => {
     res.render("index.ejs", { result: null });
+});
+
+app.post("/upload", upload.single('file_inputName'), async (req, res) => {
+  
 });
 
 app.post("/analyze", upload.single('file_inputName'), async (req, res) => {
@@ -92,8 +99,8 @@ app.post("/analyze", upload.single('file_inputName'), async (req, res) => {
       const apiPDFHandler = await axios.post(`${API_URL}?key=${API_KEY}`, payload, 
           {headers: {'Content-Type': 'application/json'}}
       );
-      console.log(apiPDFHandler.data.candidates[0].content.parts[0].text);
-        res.render("index.ejs", { result : apiPDFHandler.data.candidates[0].content.parts[0].text});
+      // console.log(apiPDFHandler.data.candidates[0].content.parts[0].text);
+      res.render("index.ejs", { result : apiPDFHandler.data.candidates[0].content.parts[0].text});
 
     } catch (error) {
         // res.render("index.ejs", { result: error.message });
